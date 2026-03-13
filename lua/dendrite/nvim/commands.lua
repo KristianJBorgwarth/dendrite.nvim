53ebada6ec2c67e4e451e2c835cae557af6b4bd7
Kristian Borgwarth <10348902@pm.me>
2026-03-14 00:36:46 +0100

    rm dead code

--- lua/dendrite/nvim/commands.lua
+++ lua/dendrite/nvim/commands.lua
@@ -1,17 +1,2 @@
-local core = require("dendrite.core.note")
-local config = require("dendrite.config")
-
 local M = {}
-
----comment create new note from template
----@param template_name string
-function M.new_note(template_name)
-end
-
-function M.search_notes_by_tag()
-end
-
-function M.goto_link()
-end
-
 return M
